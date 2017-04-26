c52079c68511a5d784cf3cff29d3d1621fb24bcc
Paulo Benatto <paulo@brandwatch.com>
2017-04-26 22:24:26 +0100

    Finished Section 4

---
+++ python/rest-api-with-flask-and-python/section4/code/security.py
@@ -0,0 +1,20 @@
+from werkzeug.security import safe_str_cmp
+from user import User
+
+users = [
+    User(1, 'ppk', 'cheirosa'),
+    User(2, 'butao', 'fidido')
+]
+
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+def identity(payload):
+    print("IDENTITY = ", payload)
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
